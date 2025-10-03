dff1605ecb45d415e0fcf3d961373ba0afce1943
pachec0js <fabio.h.l.pacheco@gmail.com>
2025-10-03 13:32:32 -0300

    Adição server funcional simples 03/10/2025

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,17 @@
+import express from 'express';
+
 const porta = process.env.PORT || 8080;
 
-console.log(`Servidor rodando na porta ${porta}`);
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(204).end();
+});
+
+const server = app.listen(porta, () => {
+  console.log(`Servidor rodando na porta ${porta}`);
+});
+
+server.on('error', (err) => {
+  console.error(`Erro ao iniciar o servidor: ${err.message}`);
+});
